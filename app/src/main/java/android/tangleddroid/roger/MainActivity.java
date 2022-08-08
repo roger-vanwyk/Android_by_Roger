6e07e6b737b9527fdf1d1391d30ca88be58fdf8a
roger-vanwyk <roger.vanwyk@gmail.com>
2022-08-08 08:10:07 +0200

    meetRogerVanWyk

--- app/src/main/java/android/tangleddroid/roger/MainActivity.java
+++ app/src/main/java/android/tangleddroid/roger/MainActivity.java
@@ -1,30 +1,38 @@
 package android.tangleddroid.roger;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-//    Initiate pressedTime
-    private long pressedTime;
-/*//Initiate boolean isAppInstalledOrNot()
-    private final boolean isAppInstalledOrNot;*/
+    /*//Initiate boolean isAppInstalledOrNot()
+        private final boolean isAppInstalledOrNot;*/
 //    Initiate FABs
     FloatingActionButton fabMain, fabCall, fabEmail, fabMeet;
+    //    Initiate pressedTime
+    private long pressedTime;
+
+    WebView webView;
+
 /*//    Initiate PackageManager
     PackageManager pm;*/
 /*//Initiate String
@@ -43,18 +51,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         checkSharedPreferences();
         setTabToViewpager();
 
-    //        Connect Views to FloatingActionButtons
-    fabMain = findViewById(R.id.fabMain);
-    fabCall = findViewById(R.id.fabCall);
-    fabEmail = findViewById(R.id.fabEmail);
-    fabMeet = findViewById(R.id.fabMeet);
+        //        Connect Views to FloatingActionButtons
+        fabMain = findViewById(R.id.fabMain);
+        fabCall = findViewById(R.id.fabCall);
+        fabEmail = findViewById(R.id.fabEmail);
+        fabMeet = findViewById(R.id.fabMeet);
 
 //        Sets onClickListener for each FAB
         fabMain.setOnClickListener(this);
         fabCall.setOnClickListener(this);
         fabEmail.setOnClickListener(this);
         fabMeet.setOnClickListener(this);
-}
+    }
 
     private void setTabToViewpager() {
         // Find the ViewPager that allows the user to swipe between fragments
@@ -114,57 +122,126 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.fabCall:
-                dialRogerPhone();
+                callRogerVanWyk();
                 break;
             case R.id.fabEmail:
-                emailRogerGmail();
+                emailRogerVanWyk();
+                break;
             case R.id.fabMeet:
-                meetRogerJitsi();
+                meetRogerVanWyk();
         }
     }
 
-        private void meetRogerJitsi () {
-            Toast.makeText(this, "Meeting with Roger", Toast.LENGTH_SHORT).show();
-/*//            Check if JitsiMeet is installed
-            if (isAppInstalledOrNot) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+        if (requestCode == 1) {
+            if (grandResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callRogerVanWyk();
+            } else {
+                Toast.makeText(this, "Oh No!! Permission Denied. Try Again!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void meetRogerVanWyk() {
+        Toast.makeText(this, "Meeting with Roger", Toast.LENGTH_SHORT).show();
+
+        PackageManager pm = getPackageManager();
+        boolean installed = false;
+
+        while (!installed) {
+            installed = appInstalledOrNot("com.jitsi.meeu");
+            if (installed) {
+                Toast.makeText(this, "App installed", Toast.LENGTH_SHORT).show();
 
                 Log.i("RogDroidLog", "Application is already installed");
 
                 //                This Intent launch the package if it's already installed on device
-                Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("com.jitsi.meeu");
-                startActivity(LaunchIntent);
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.jitsi.meeu");
+                launchIntent = new Intent(MainActivity.this, (getPackageManager().getLaunchIntentForPackage("com.jitsi.meeu")).getClass());
+                startActivity(launchIntent);
+
                 return;
             } else {
 //                Download and install application
                 Intent installJitsi = new Intent("android.intent.action.VIEW");
-                installJitsi.setData(Uri.parse((String) "https://drive.google.com/file/d/1J1m6IjPES8bhez9i_epojXD6PzWMSHb7/view?usp=drivesdk"));
+                installJitsi.setData(Uri.parse("https://drive.google.com/file/d/1J1m6IjPES8bhez9i_epojXD6PzWMSHb7/view?usp=drivesdk"));
                 startActivity(installJitsi);
                 Log.i("RogDroidLog", "Require JitsiMeet app to be installed");
             }
-            pm = getPackageManager();
             try {
 
-                pm.getPackageInfo(s, PackageManager.GET_ACTIVITIES);
-            } catch (PackageManager.NameNotFoundException ignored) {}
-        }*/
-        }
-
-        private void emailRogerGmail () {
-            Toast.makeText(this, "Emailing Roger", Toast.LENGTH_SHORT).show();
-            Intent compileEmail = new Intent(MainActivity.this, EmailActivity.class);
-            startActivity(compileEmail);
-        }
-
-        private void dialRogerPhone () {
-            Toast.makeText(this, "Calling Roger", Toast.LENGTH_SHORT).show();
-            try {
-                startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "0789871987", null)));
-            } finally {
-                startActivity(new Intent(Intent.ACTION_CALL, Uri.fromParts("tel", "0789871987", null)));
+                pm.getPackageInfo("com.jitsi.meeu", PackageManager.GET_ACTIVITIES);
+            } catch (PackageManager.NameNotFoundException ignored) {
             }
+            startActivity(getPackageManager().getLaunchIntentForPackage("com.jitsi.meeu"));
+        }
+        return;
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed = false;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+
+        /*String html = "<!DOCTYPE html>\n" +
+                "    <html>\n" +
+                "      <head>\n" +
+                "        <script src='https://8x8.vc/external_api.js' async></script>\n" +
+                "        <style>html, body, #jaas-container { height: 100%; }</style>\n" +
+                "        <script type=\"text/javascript\">\n" +
+                "          window.onload = () => {\n" +
+                "            const api = new JitsiMeetExternalAPI(\"8x8.vc\", {\n" +
+                "              roomName: \"vpaas-magic-cookie-b5b47f64f28a44178840f264e2b05986/AndroidByRogerJitsiMeeting\",\n" +
+                "              parentNode: document.querySelector('#jaas-container')\n" +
+                "            });\n" +
+                "          }\n" +
+                "        </script>\n" +
+                "      </head>\n" +
+                "      <body><div id=\"jaas-container\" /></body>\n" +
+                "    </html>";
+
+        webView.loadData(html, "text/javascript", null);*/
+    }
+
+    private void emailRogerVanWyk() {
+        Toast.makeText(this, "Emailing Roger", Toast.LENGTH_SHORT).show();
+        Intent compileEmail = new Intent(Intent.ACTION_SEND);
+        compileEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"roger.vanwyk@gmail.com"});
+        compileEmail.putExtra(Intent.EXTRA_SUBJECT, "Android_By_Roger Android Portfolio");
+        compileEmail.setType("message/rfc822");
+        startActivity(Intent.createChooser(compileEmail, "Choose email app"));
+    }
+
+    private void callRogerVanWyk() {
+        Toast.makeText(this, "Calling Roger", Toast.LENGTH_SHORT).show();
+
+        Intent callRogerIntent = new Intent(Intent.ACTION_CALL);
+
+        callRogerIntent.setData(Uri.parse("tel: 0789871987"));
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+        } else {
+
+            startActivity(callRogerIntent);
+            finish();       //  Without this, call would be re-occurring...
         }
 
-//    Sets doubleBackPress to exit
+        requestPermission();
+
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+    }
+
+    //    Sets doubleBackPress to exit
     @Override
     public void onBackPressed() {
 
